4ac4a8c1f568fbd3274fc81fc01f5bdb7bc6c03d
sruddadi <srikaruddadi@gmail.com>
2023-06-26 13:17:12 -0500

    Implemented HashSet Basic topics

---
+++ HashSetBasics/src/IterateHashSet.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class IterateHashSet {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>(Arrays.asList("Srikar", "Uddadi", "Mani", "Kandan", "Venkataraman", "Manasa", "Manthena", "Vaishnav", "Koratpalli", "Sritej"));
+
+        System.out.println("Using For Each");
+        set.forEach(x -> System.out.print(x + " "));
+        System.out.println();
+        System.out.println("Using Stream");
+        set.stream().forEach(x -> System.out.print(x + " "));
+        System.out.println();
+        System.out.println("Using Iterator");
+        Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next()+ " ");
+        }
+    }
+}
